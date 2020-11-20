51088606add3bd574e63a0376eef690b8b603b04
maxima1ist <stepura.max@mail.ru>
2020-11-20 23:21:14 +0300

    hw-7

---
+++ hw_7_meanwhile_in_the_seventh_galaxy/first_variant/cmake-build-debug/CMakeFiles/meanwhile_in_the_seventh_galaxy_first_variant.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/maxima1ist/Programs and projects/MADE/Advanced C++/ДЗ/made-cpp-2020-hw/hw_7_meanwhile_in_the_seventh_galaxy/first_variant/src/main.cpp" "/media/maxima1ist/Programs and projects/MADE/Advanced C++/ДЗ/made-cpp-2020-hw/hw_7_meanwhile_in_the_seventh_galaxy/first_variant/cmake-build-debug/CMakeFiles/meanwhile_in_the_seventh_galaxy_first_variant.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw_7_meanwhile_in_the_seventh_galaxy/first_variant/cmake-build-debug/CMakeFiles/meanwhile_in_the_seventh_galaxy_first_variant.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meanwhile_in_the_seventh_galaxy_first_variant.dir/src/main.cpp.o"
+  "meanwhile_in_the_seventh_galaxy_first_variant"
+  "meanwhile_in_the_seventh_galaxy_first_variant.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/meanwhile_in_the_seventh_galaxy_first_variant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
