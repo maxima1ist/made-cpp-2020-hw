e95d01bdaf8cc1fe2ecd1c20e1032ae84d3901ae
maxima1ist <stepura.max@mail.ru>
2020-12-02 14:13:31 +0300

    fixed version

--- proj_4_functional/src/compose.h
+++ proj_4_functional/src/compose.h
@@ -1,70 +1,26 @@
 #pragma once
+#include <functional>
 
-template<class Func, typename ...Other>
-struct Composer {
-public:
-    typedef Func returnFunc;
+template<typename ReturnType, typename ...Argument>
+std::function<ReturnType(Argument...)> compose(const std::function<ReturnType(Argument...)>& func) {
+    return func;
+}
 
-private:
-    Func func;
-    Composer<Other...> other;
+template<typename ReturnType1, typename TheSameType, typename ...Argument2>
+std::function<ReturnType1(Argument2...)> compose(
+    const std::function<ReturnType1(TheSameType)>& func1,
+    const std::function<TheSameType(Argument2...)>& func2
+) {
+    std::function<ReturnType1(Argument2...)> result = [func1, func2](auto ...arg) { return func1(func2(arg...)); };
+    return result;
+}
 
-public:
-    explicit Composer(Func func, Other ...other) : func(func), other(other...) {
-        static_assert(std::is_same<
-            typename Composer<Other...>::returnFunc::result_type,
-            typename Func::argument_type
-        >::value, "types error!");
-    }
-
-    template<class Arg>
-    auto operator()(const Arg& arg) {
-        return func(other(arg));
-    }
-};
-
-template<class Func1, class Func2>
-struct Composer<Func1, Func2> {
-public:
-    typedef Func1 returnFunc;
-
-private:
-    Func1 func1;
-    Func2 func2;
-
-public:
-    explicit Composer(Func1 func1, Func2 func2) : func1(func1), func2(func2) {
-        static_assert(std::is_same<
-            typename Func2::result_type,
-            typename Func1::argument_type
-        >::value, "types error!");
-    }
-
-    template<class Arg>
-    auto operator()(const Arg& arg) {
-        return func1(func2(arg));
-    }
-};
-
-template<class Func>
-struct Composer<Func> {
-private:
-    Func func;
-
-public:
-    explicit Composer(Func func) : func(func) {}
-
-    //about ...
-    //may be excessively, but it's really logical, that the only one can be with a variable number of arguments
-    //cause it's just one function, not a compose
-    //but if it was not true logic it would be really easy to fix just erasing "..." everywhere
-    template<class ...Arg>
-    auto operator()(const Arg& ...arg) {
-        return func(arg...);
-    }
-};
-
-template<class ...Funcs>
-auto compose(Funcs ...funcs) {
-    return Composer<Funcs...>(funcs...);
+template<typename ReturnType1, typename TheSameType, typename Argument2, typename ...Other>
+std::function<ReturnType1(Argument2)> compose(
+    const std::function<ReturnType1(TheSameType)>& func1,
+    const std::function<TheSameType(Argument2)>& func2,
+    Other ...other
+) {
+    std::function<ReturnType1(Argument2)> result = [func1, func2](auto arg) { return func1(func2(arg)); };
+    return compose(result, other...);
 }
